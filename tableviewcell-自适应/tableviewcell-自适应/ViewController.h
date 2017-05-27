52d906a32d62685dce1329343e24e4e47dadc255
quakoo <quakoo@quakoodeiMac.local>
2017-05-27 09:44:10 +0800

    创建工程

---
+++ tableviewcell-自适应/tableviewcell-自适应/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  tableviewcell-自适应
+//
+//  Created by quakoo on 2017/5/27.
+//  Copyright © 2017年 quakoo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ tableviewcell-自适应/tableviewcell-自适应/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  tableviewcell-自适应
+//
+//  Created by quakoo on 2017/5/27.
+//  Copyright © 2017年 quakoo. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
